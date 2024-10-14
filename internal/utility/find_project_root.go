62f100a9d26edae10093e82993043754f7532786
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-10-14 03:50:33 +0300

    feat: add find_project_root utility

---
+++ internal/utility/find_project_root.go
@@ -0,0 +1,25 @@
+package utility
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func FindProjectRoot() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir, nil
+		}
+
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			return "", os.ErrNotExist
+		}
+		dir = parentDir
+	}
+}
